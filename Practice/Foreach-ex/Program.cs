9de65ce4232a1d042841e9ac97944545cb639851
Park-JuHyeon <j-hyeon97@naver.com>
2023-06-06 18:49:44 +0900

    0605 연습

---
+++ Practice/Foreach-ex/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach_ex
+{
+    // foreach란 끝을 지정해주는 다른 반복문과 달리, 인자로 들어온 itrable-itme의 내부 인덱스 끝까지
+    // 알아서 순환 해주는 반복문
+
+    // 에제 1 : 1차원 배열에서의 foreach
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Console.WriteLine("##foreach 사용!!");
+            int index = 0;
+            foreach (int elem in arr)
+            {
+                Console.WriteLine("arr[" + index + "] : " + elem);
+                ++index;
+            }
+
+            Console.WriteLine("current i : " + index);
+
+            // for문 작성시
+            Console.WriteLine();
+            Console.WriteLine("## for문 사용!!");
+            for(int i=0; i < 8; i++)
+            {
+                Console.WriteLine("arr[" + i + "] : " + arr[i]);
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CSharp-Practice
+
+## Day 01
+- Foreach문 예제 연습
+	- 예제1 : 1차원 배열에서의 foreach
