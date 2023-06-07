d061804dfa68a62d11aca9ff4821831432b2f25f
Park-JuHyeon <j-hyeon97@naver.com>
2023-06-07 23:58:02 +0900

    0607 c# 공부
    foreach

--- Practice/Foreach-ex/Program.cs
+++ Practice/Foreach-ex/Program.cs
@@ -12,28 +12,96 @@ namespace Foreach_ex
     // 에제 1 : 1차원 배열에서의 foreach
     public class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        //    Console.WriteLine("##foreach 사용!!");
+        //    int index = 0;
+        //    foreach (int elem in arr)
+        //    {
+        //        Console.WriteLine("arr[" + index + "] : " + elem);
+        //        ++index;
+        //    }
+
+        //    Console.WriteLine("current i : " + index);
+
+        //    // for문 작성시
+        //    Console.WriteLine();
+        //    Console.WriteLine("## for문 사용!!");
+        //    for(int i=0; i < 8; i++)
+        //    {
+        //        Console.WriteLine("arr[" + i + "] : " + arr[i]);
+        //    }
+
+        //}
+
+        // 예제2 : 2차원 배열에서의 foreach
+        //static void Main(string[] args)
+        //{
+        //    int[,] arr = new int[4, 3]
+        //    {
+        //        {11, 12, 13 },
+        //        {21, 22, 23 },
+        //        {32, 32, 33 },
+        //        {41, 42, 43 }
+        //    };
+
+        //    Console.WriteLine("foreach문 시작!!");
+        //    int index = 0;
+        //    foreach (int elem in arr)
+        //    {
+        //        Console.WriteLine("arr[" + index + "] : " + elem);
+        //        ++index;
+        //    }
+        //    Console.WriteLine("current i : " + index);
+
+        //    // for문으로 사용시
+        //    Console.WriteLine();
+        //    Console.WriteLine("## for문 사용!!");
+
+        //    for(int i = 0; i <4; i++)
+        //    {
+        //        for(int j = 0; j <3; j++)
+        //        {
+        //            Console.WriteLine("arr[" + i + "," + j + "] :" + arr[i, j]);
+        //        }
+        //    }
+        //}
+
+        // enum을 순회할 수 있는 foreach
+        enum Month
+        {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            Setember,
+            October,
+            November,
+            December
+        };
+
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
-
-            Console.WriteLine("##foreach 사용!!");
-            int index = 0;
-            foreach (int elem in arr)
+            Console.WriteLine("## foreach int 열거형 출력! - Enum.GetValues()");
+            foreach (int elem in Enum.GetValues(typeof(Month)))
             {
-                Console.WriteLine("arr[" + index + "] : " + elem);
-                ++index;
+                Console.WriteLine(elem + "월");
             }
 
-            Console.WriteLine("current i : " + index);
-
-            // for문 작성시
             Console.WriteLine();
-            Console.WriteLine("## for문 사용!!");
-            for(int i=0; i < 8; i++)
+            Console.WriteLine("## foreach string 열거형 출력! - Enum.GetValues()");
+            foreach(string elem in  Enum.GetNames(typeof(Month)))
             {
-                Console.WriteLine("arr[" + i + "] : " + arr[i]);
+                Console.WriteLine(elem);
             }
-
         }
     }
+
+
 }
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # CSharp-Practice
 
-## Day 01
+## Day 01 (6월 5일)
 - Foreach문 예제 연습
 	- 예제1 : 1차원 배열에서의 foreach
+	
+## Day 02 (6월 7일)
+- Foreach문 예제 연습
+	- 예제2 : 2차원 배열에서의 foreach
+	- 예제3 : enum을 사용한 foreach
